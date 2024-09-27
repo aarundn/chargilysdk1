992deed20dab6a18970efef1eb102ae65d58d4c2
MCD <debchouneharoun@gmail.com>
2024-09-27 19:52:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/chargilysdk/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.chargilysdk
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+}
